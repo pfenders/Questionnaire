5c9450cf40a4f44f6ef0eeb048f37bbce49e1b3d
pfenders <pfenders3123@gmail.com>
2019-08-01 03:17:19 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,102 +4,53 @@ const quizPanel = $("#quiz-area");
 const questions = [
     {
         question: "What percentage of school leavers and graduates believe that it is important to have fun at work?",
-        answer: {
-            a: "50%",
-            b: "32%",
-            c: "79%",
-            d: "83%"
-        },
+        answer: ["50%", "32%", "79%", "83%"],
         correctAns: "79%"
     },
     {
         question: "During thinking, how much percentage of our brains do we use?",
-        answer: {
-            a: "71%",
-            b: "35%",
-            c: "58%",
-            d: "40%"
-        },
+        answer: ["71%", "35%", "58%","40%"],
         correctAns: "35%"
     },
     {
         question: "What country was paper invented in?",
-        answer: {
-            a: "China",
-            b: "United States",
-            c: "England",
-            d: "Japan"
-        },
+        answer: ["China", "United States", "England","Japan"
+        ],
         correctAns: "China"
     },
     {
         question: "According to Maslow’s hierarchy of needs what is the final employees need after completing all of the others?",
-        answer: {
-            a: "Love and Belonging",
-            b: "Safety",
-            c: "Self-Actualization",
-            d: "Psychological Needs"
-        },
+        answer: ["Love and Belonging", "Safety", "Self-Actualization", "Psychological Needs"],
         correctAns: "Self-Actualization"
     },
     {
         question: "What percentage of workers believe having access to social media platforms helps them to be more productive?",
-        answer: {
-            a: "20%",
-            b: "53%",
-            c: "65%",
-            d: "39%"
-        },
+        answer: ["20%", "53%","65%", "39%"],
         correctAns: "39%"
     },
     {
         question: "Which company allows employees to bring their dogs to the work?",
-        answer: {
-            a: "Ikea",
-            b: "Amazon",
-            c: "Microsoft",
-            d: "Samsung"
-        },
+        answer: [ "Ikea", "Amazon", "Microsoft", "Samsung"],
         correctAns: "Amazon"
     },
     {
         question: "How many minutes a day does the average office worker spend looking for lost files and other items?",
-        answer: {
-            a: "50 Minutes",
-            b: "20 Minutes",
-            c: "15 Minutes",
-            d: "35 Minutes"
-        },
+        answer: ["50 Minutes", "20 Minutes", "15 Minutes", "35 Minutes" ],
         correctAns: "50 Minutes"
     },
     {
         question: "During an average day, how many miles does a typist’s fingers travel?",
-        answer: {
-            a: "6 Miles",
-            b: "3.5 Miles",
-            c: "12.6 Miles",
-            d: "9.7 Miles"
-        },
+        answer: ["6 Miles", "3.5 Miles", "12.6 Miles", "9.7 Miles"],
         correctAns: "12.6 Miles"
     },
     {
         question: "When was Microsoft established?",
-        answer: {
-            a: "2005",
-            b: "1998",
-            c: "1965",
-            d: "1975"
-        },
+        answer: ["2005", "1998", "1965", "1975"],
         correctAns: "1975"
     },
     {
         question: "Who is the owner of Microsoft?",
-        answer: {
-            a: "Bill Clinton",
-            b: "Robert Half",
-            c: "Mr Clean",
-            d: "Bill Gates"
-        },
+        answer: ["Bill Clinton", "Robert Half", "Mr Clean", "Bill Gates"],
         correctAns: "Bill Gates"
     },
 ];
